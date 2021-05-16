3007629f1e7753e03da9cfce2b18de736d806852
Dafr <dafr.prf@gmail.com>
2021-05-16 17:21:21 +0200

    Added slideAnimation function wich makes slides move left and right

--- frontend/slide.js
+++ frontend/slide.js
@@ -1,15 +1,15 @@
-const slider = document.querySelector('.products');
+const slide = document.querySelector('.products');
 
-const updateCard = (product) => {
+function updateCard(product) {
   for (let i = 0; i < product.length; i++) {
     const name = product[i].name;
     const price = product[i].price;
     const image = product[i].imageUrl;
-    const lenses = product[i].lenses;
-    const description = product[i].description;
+    // const lenses = product[i].lenses;
+    // const description = product[i].description;
 
-    slider.innerHTML += `
-        <a href="./product.html">
+    slide.innerHTML += `
+        <a id="card" class="products__card${i}" href="./product.html">
             <figure class="products__card">
                 <img class="products__image" src="${image}" alt="" />
                 <figcaption class="products__description">
@@ -25,6 +25,21 @@ const updateCard = (product) => {
                 </figcaption>
             </figure>
         </a>
-    `;
+        `;
   }
-};
+}
+
+function slideAnimation() {
+  const arrowLeft = document.querySelector('.arrows-left');
+  const arrowRight = document.querySelector('.arrows-right');
+
+  arrowLeft.addEventListener('click', () => {
+    slide.scrollLeft -= 384 * 2;
+  });
+
+  arrowRight.addEventListener('click', () => {
+    slide.scrollLeft += 384 * 2;
+  });
+}
+
+slideAnimation();
